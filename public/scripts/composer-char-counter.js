f4fdc06c54e655d2aa23f392bd3709067caea60a
Jimyung Park <pzy51992@hotmail.com>
2018-12-12 04:16:16 +0000

    set textarea's input will be counted and displays, created seperate nav-bar css and new-tweet css files

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  console.log('DOM is Ready!');
+
+
+
+  $('.new-tweet form textarea').on('input', function(event){
+    let counter = 140;
+    let inputLength = this.value.length;
+    let textLeft = counter - inputLength;
+    let currentCount = $(this).parent().children('.counter');
+
+    currentCount.text(textLeft);
+
+    if(textLeft < 0) {
+      $(currentCount).removeClass('nCounter');
+    }
+  });
+});
+
+
+
